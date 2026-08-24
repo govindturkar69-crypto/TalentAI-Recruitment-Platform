4f58a3fdcc4dfc3f344c2a5563a0cccc75222b0d
Govind Turkar <govindturkar69@gmail.com>
2026-08-24 18:26:30 +0530

    test: fix test_save_job mock assertions

--- tests/test_candidate_phase2.py
+++ tests/test_candidate_phase2.py
@@ -316,8 +316,24 @@ def test_save_job(client, mock_db):
     assert response.status_code == 302
 
     calls = mock_db.execute.call_args_list
-    assert "INSERT INTO saved_jobs" in calls[1][0][0]
-    assert calls[1][0][1] == (3, 1)
+
+    job_check_calls = [
+        call
+        for call in calls
+        if "SELECT id FROM jobs WHERE id = %s" in call.args[0]
+    ]
+
+    assert len(job_check_calls) == 1
+    assert job_check_calls[0].args[1] == (1,)
+
+    insert_calls = [
+        call
+        for call in calls
+        if "INSERT INTO saved_jobs" in call.args[0]
+    ]
+
+    assert len(insert_calls) == 1
+    assert insert_calls[0].args[1] == (3, 1)
 
 
 def test_unsave_job(client, mock_db):
